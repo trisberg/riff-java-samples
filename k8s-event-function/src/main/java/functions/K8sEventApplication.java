21df73a1e7893b89fc75155c9eef2f59d43e7138
trisberg <trisberg@pivotal.io>
2020-02-25 09:21:23 -0500

    Modified parsing of cloud events

--- k8s-event-function/src/main/java/functions/K8sEventApplication.java
+++ k8s-event-function/src/main/java/functions/K8sEventApplication.java
@@ -1,12 +1,13 @@
 package functions;
 
+import java.net.URI;
+import java.time.ZonedDateTime;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.function.Function;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.salaboy.cloudevents.helper.CloudEventsHelper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +19,7 @@ import org.springframework.messaging.support.MessageBuilder;
 
 import io.cloudevents.CloudEvent;
 import io.cloudevents.v03.AttributesImpl;
+import io.cloudevents.v03.CloudEventBuilder;
 
 @SpringBootApplication
 public class K8sEventApplication {
@@ -35,25 +37,19 @@ public class K8sEventApplication {
 					headers.put(key, in.getHeaders().get(key).toString());
 				}
 			}
-			CloudEvent<AttributesImpl, String> cloudEvent = 
-					CloudEventsHelper.parseFromRequest(headers, (Object) in.getPayload());
+			CloudEvent<AttributesImpl, JsonNode> cloudEvent = parseCloudEvent(headers, in.getPayload());
 			String results = handleCloudEvent(cloudEvent);
 			return MessageBuilder.withPayload(results).build();
 		};
 	}
 
-	private String handleCloudEvent(CloudEvent<AttributesImpl, String> cloudEvent) {
+	private String handleCloudEvent(CloudEvent<AttributesImpl, JsonNode> cloudEvent) {
 		log.info("CLOUD EVENT ATTR: " + cloudEvent.getAttributes());
 		log.info("CLOUD EVENT DATA: " + cloudEvent.getData());
 		final String id = cloudEvent.getAttributes().getId();
 		final String type = cloudEvent.getAttributes().getType();
 		final String subject = cloudEvent.getAttributes().getSubject().orElse("");
-		JsonNode node = null;
-		try {
-			node = mapper.readTree(cloudEvent.getData().get());
-		} catch (Exception e) {
-			log.info("No JSON Data: " + cloudEvent.getData());
-		}
+		JsonNode node = cloudEvent.getData().orElse(null);
 		if (node != null) {
 			final JsonNode version = node.get("apiVersion");
 			log.info("VERSION: " + version);
@@ -67,10 +63,32 @@ public class K8sEventApplication {
 			final JsonNode reason = node.get("reason");
 			final JsonNode message = node.get("message");
 			log.info("REASON: " + reason +":" + message);
+		} else {
+			log.info("No JSON Data: " + cloudEvent.getData());
 		}
 		log.info("MESSAGE: " + "Processed " + type + ":" + subject + ":" + id);
 		return "Processed Event " + type + ":" + subject + ":" + id;
 	}
+
+    private static CloudEvent<AttributesImpl, JsonNode> parseCloudEvent(Map<String, String> headers, JsonNode body) throws IllegalStateException {
+        final String CE_ID = "ce-id";
+		final String CE_TYPE = "ce-type";
+		final String CE_SOURCE = "ce-source";
+		final String CE_TIME = "ce-time";
+		final String CE_DATACONTENTTYPE = "ce-datacontenttype";
+
+	    if (headers.get(CE_ID) == null || (headers.get(CE_SOURCE) == null || headers.get(CE_TYPE) == null)) {
+            throw new IllegalStateException("Cloud Event required fields are not present.");
+        }
+
+        return CloudEventBuilder.<JsonNode>builder().withId(headers.get(CE_ID))
+                .withType(headers.get(CE_TYPE))
+                .withSource((headers.get(CE_SOURCE) != null) ? URI.create(headers.get(CE_SOURCE)) : null)
+                .withTime((headers.get(CE_TIME) != null) ? ZonedDateTime.parse(headers.get(CE_TIME)) : null)
+                .withData((body != null) ? body : null)
+                .withDatacontenttype((headers.get(CE_DATACONTENTTYPE) != null) ? headers.get(CE_DATACONTENTTYPE) : "application/json")
+                .build();
+    }
 
 	public static void main(final String[] args) {
 		SpringApplication.run(K8sEventApplication.class, args);
